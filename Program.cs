ef4c90efd08d097be7f480c4788f395cac9699bc
Abdalazez-Azmi <90723420+Abdalazez-Azmi@users.noreply.github.com>
2022-02-11 20:19:15 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ODD || Even Number
+
+            int x;
+            x = Convert.ToInt16(Console.ReadLine());
+            if (x%2 == 0)
+            {
+                Console.WriteLine("Number " + x + " is Even Number ");
+            }
+            else
+            {
+                Console.WriteLine("Number " + x + " is Odd Number ");
+
+            }
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
